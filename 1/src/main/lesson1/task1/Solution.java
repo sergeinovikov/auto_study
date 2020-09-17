63444ec05c2adca7dd7c2f71f7545658df5f14eb
sergeinovikov <68592863+sergeinovikov@users.noreply.github.com>
2020-09-17 21:40:27 +0400

    Класс с методом, принимающим целочисленное значение radius, который возвращает площадь окружности

--- 1/src/main/lesson1/task1/Solution.java
+++ 1/src/main/lesson1/task1/Solution.java
@@ -3,9 +3,9 @@ package main.lesson1.task1;
 public class Solution {
     public static void main(String[] args) {
         Solution Circle = new Solution();
-        System.out.println(Circle.areaOfACircle(2));
+        System.out.println(Circle.areaOfACircle(100));
         }
     double areaOfACircle(int radius) {
-        return radius*2*3.14;
+        return 3.14*radius*radius;
     }
 }
