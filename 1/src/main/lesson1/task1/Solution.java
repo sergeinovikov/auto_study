4290e6adc8a4a809fd5886118312234a85e10627
sergeinovikov <68592863+sergeinovikov@users.noreply.github.com>
2020-09-17 21:34:31 +0400

    Метод с площадью круга

--- 1/src/main/lesson1/task1/Solution.java
+++ 1/src/main/lesson1/task1/Solution.java
@@ -1,2 +1,11 @@
-package main.lesson1.task1;public class Solution {
+package main.lesson1.task1;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution Circle = new Solution();
+        System.out.println(Circle.areaOfACircle(2));
+        }
+    double areaOfACircle(int radius) {
+        return radius*2*3.14;
+    }
 }
