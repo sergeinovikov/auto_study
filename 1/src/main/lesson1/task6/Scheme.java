d0bbe74cba6733378e1ca49ca340721fc9198f4f
sergeinovikov <68592863+sergeinovikov@users.noreply.github.com>
2020-09-19 13:31:12 +0400

    Добавил все примеры в задания

---
+++ 1/src/main/lesson1/task6/Scheme.java
@@ -0,0 +1,16 @@
+package main.lesson1.task6;
+
+public class Scheme {
+    public static void main(String[] args) {
+        Scheme schemeResult = new Scheme();
+        System.out.println(schemeResult.result(false, false, false, false));
+        System.out.println(schemeResult.result(true, true, true, true));
+        System.out.println(schemeResult.result(false, true, false, true));
+        System.out.println(schemeResult.result(true, false, true, false));
+        System.out.println(schemeResult.result(true, true, false, false));
+    }
+
+    boolean result (boolean a, boolean b, boolean c, boolean d) {
+        return (a&&b)&&!(c||d);
+    }
+}
