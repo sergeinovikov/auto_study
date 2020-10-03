c45ece3960346f83c16e26c5508f5990ac238176
Sergei Novikov <github@sergeynovi.ru>
2020-10-03 16:17:52 +0300

    Задача, где создан класс Human с именем, фамилией и датой рождения. Конструктор задан по умолчанию. Все переменные класса Human приватные и их можно получить на печать/изменить через геттеры и сеттеры. В сеттерах идёт проверка в соответствии с условиями задачи: -фамилия/имя - не меньше 3 кирилицы; -первый символ фамилии/имени- заглавная буква; -дата не больше текущей. В случае установки нового значения/получения переменной на экран выводится сообщение в зависиммости от прохождения валидации.
    Примере в классе main взяты из готовой работы (лень было))

---
+++ 1/src/main/lesson4/task1/Human.java
@@ -0,0 +1,77 @@
+package main.lesson4.task1;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Human {
+    private String lastName;
+    private String firstName;
+    private Date birthDate;
+
+    public void setLastName(String newLastName) {
+        if (newLastName.matches("^([А-Я-Ё])([а-я-ё]{2,})$")) {
+            this.lastName = newLastName;
+            setCorrectPrint(this.lastName);
+        } else {
+            setIncorrectPrint(newLastName);
+        }
+    }
+
+    public String getLastName() {
+        getPrint(lastName);
+        return lastName;
+    }
+
+    public void setFirstName(String newFirstName) {
+        if (newFirstName.matches("^([А-Я-Ё])([а-я-ё]{2,})$")) {
+            this.firstName = newFirstName;
+            setCorrectPrint(this.firstName);
+        } else {
+            setIncorrectPrint(newFirstName);
+        }
+    }
+
+    public String getFirstName() {
+        getPrint(firstName);
+        return firstName;
+    }
+
+    public void setBirthDate(Date newBirthDate) {
+        Date currentDay = new Date();
+        if (newBirthDate.before(currentDay)) {
+            this.birthDate = newBirthDate;
+            setCorrectPrint(this.birthDate);
+        } else {
+            setIncorrectPrint(newBirthDate);
+        }
+    }
+
+    public Date getBirthDate() {
+        getPrint(birthDate);
+        return birthDate;
+    }
+
+    public void getPrint(Object textToPrint) {
+        if (textToPrint == this.lastName) {
+            System.out.printf("Получено значение фамилии: %s%n", textToPrint);
+        } else if (textToPrint == this.firstName) {
+            System.out.printf("Получено значение имени: %s%n", textToPrint);
+        } else if (textToPrint == this.birthDate) {
+            System.out.printf("Получено значение даты рождения: %s%n", textToPrint);
+        }
+    }
+
+    public void setCorrectPrint(Object textToPrint) {
+        if (textToPrint == this.lastName) {
+            System.out.printf("Установлено корректное значение фамилии: %s%n", textToPrint);
+        } else if (textToPrint == this.firstName) {
+            System.out.printf("Установлено корректное значение имени: %s%n", textToPrint);
+        } else if (textToPrint == this.birthDate) {
+            System.out.printf("Установлено корректное значение даты рождения: %s%n", textToPrint);
+        }
+    }
+
+    public void setIncorrectPrint(Object textToPrint) {
+        System.out.printf("Введенное значение некорректно: %s%n", textToPrint);
+    }
+}
