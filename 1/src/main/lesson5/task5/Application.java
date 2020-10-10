04de85be6ec21b878f96fcd984baabca6be4f79e
Sergei Novikov <github@sergeynovi.ru>
2020-10-10 16:34:45 +0300

    Класс Passport, содержит поля String series, String number. Конструктор класса Passport принимает два параметра и выбрасывает соответствующие исключения, если возникают соответствующие условия (схема из задачи 5, в задании 5)

--- 1/src/main/lesson5/task5/Application.java
+++ 1/src/main/lesson5/task5/Application.java
@@ -2,19 +2,26 @@ package main.lesson5.task5;
 
 public class Application {
     public static void main(String[] args) {
-        Application passportSoftware = new Application();
-        passportSoftware.createPassport("22 22", "123456");
-        passportSoftware.createPassport("22 2", "123456");
-        passportSoftware.createPassport("22 2", "1234567");
-        passportSoftware.createPassport(null, "123456");
-        passportSoftware.createPassport("22 22", null);
-        passportSoftware.createPassport("22 22", "123456");
-        passportSoftware.createPassport("11 11", "123457");
-        passportSoftware.createPassport("11 11", "123457");
+        createPassport("22 22", "123456");
+        createPassport("22 2", "123456");
+        createPassport("22 22", "1234567");
+        createPassport(null, "123456");
+        createPassport("22 22", null);
+        createPassport("22 22", "123456");
+        createPassport("11 11", "123457");
+        createPassport("11 11", "123457");
     }
 
-    public Passport createPassport(String series, String number) {
-        Passport passport = new Passport(series, number);
-        return passport.getPassport();
+    private static Passport createPassport(String series, String number) {
+        try {
+            return new Passport(series, number);
+        } catch (PassportSeriesArgumentException | PassportNumberArgumentException |
+                PassportSeriesNullPointerException | PassportNumberNullPointerException exception) {
+            System.out.println("Введите верные значения серии и номера паспорта");
+            return null;
+        } catch (PassportAlreadyExistsException exception) {
+            System.out.println("Данные серия и номер паспорта уже существуют");
+            return null;
+        }
     }
 }
--- 1/src/main/lesson5/task5/Passport.java
+++ 1/src/main/lesson5/task5/Passport.java
@@ -6,16 +6,13 @@ public class Passport {
     private static PassportData archive;
 
     public Passport(String newSeries, String newNumber) {
-        try {
             if (newSeries != null) {
                 if (newSeries.matches("^\\d{2}\\s\\d{2}$")) {
-                    this.series = newSeries;
+                    series = newSeries;
                 } else {
-                    System.out.println("Введите верные значения серии и номера паспорта");
                     throw new PassportSeriesArgumentException();
                 }
             } else {
-                System.out.println("В конструктор в качестве серии паспорта передан null");
                 throw new PassportSeriesNullPointerException();
             }
             if (newNumber != null) {
@@ -25,25 +22,16 @@ public class Passport {
                     if (!isContains) {
                         series = newSeries;
                         number = newNumber;
+                        System.out.printf("Создан новый паспорт. Данные серии:%s, данные номера:%s%n", series, number);
                     } else {
-                        System.out.println("Данные серия и номер паспорта уже существуют");
                         throw new PassportAlreadyExistsException();
                     }
                 } else {
-                    System.out.println("Введите верные значения серии и номера паспорта");
                     throw new PassportNumberArgumentException();
                 }
             } else {
-                System.out.println("В конструктор в качестве номера паспорта передан null");
                 throw new PassportNumberNullPointerException();
             }
-        } catch (PassportIllegalArgumentException illegalArgumentException) {
-            System.out.println("Ошибка ввода серии или номера паспорта");
-        } catch (NullPointerException nullPointerException) {
-            System.out.println("Ошибка - отсутствует ввод ссерии или номера паспорта");
-        } catch (PassportAlreadyExistsException existsException) {
-            System.out.println("Ошибка - повтор паспорта");
-        }
     }
 
 
@@ -54,15 +42,4 @@ public class Passport {
         return archive;
     }
 
-    Passport getPassport() {
-        try {
-            if (this.series == null || this.number == null) {
-                throw new NullPointerException();
-            }
-            System.out.printf("Серия:%s, номер:%s%n", this.series, this.number);
-            return this;
-        } catch (NullPointerException nullPointerException) {
-            return null;
-        }
-    }
 }
