a132bcd80cd20e98f0b632a7d8134c04e4486431
Sergei Novikov <github@sergeynovi.ru>
2020-10-14 15:15:23 +0300

    Добавил сортировку по убыванию. Задача, в которой генерируется случайное число уроков (от 4 до 6) для ученика на каждый день недель (с понедельника по субботу). Типы уроков и дни недели перечислены в Enum. Далее на экран для кажого дня выводится список случайных уроков и ниже считается количество уроков за неделю по каждому типу урока.

--- 1/src/main/lesson6/task3/Application.java
+++ 1/src/main/lesson6/task3/Application.java
@@ -1,11 +1,12 @@
 package main.lesson6.task3;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class Application {
+    private static boolean DESC = false;
+
     public static void main(String[] args) {
         Scholar boy = new Scholar();
 
@@ -38,8 +39,25 @@ public class Application {
             amountOfLessons.put(currentLessonType, currentLessonAmount);
         }
 
-        for (Lesson currentLessonType : Lesson.values()) {
-            System.out.printf("%s : %s%n", currentLessonType.getDescription(), amountOfLessons.get(currentLessonType));
-        }
+        Map<Lesson, Integer> sortedMapDesc = sortByValue(amountOfLessons, DESC);
+        printMap(sortedMapDesc);
+    }
+
+    private static Map<Lesson, Integer> sortByValue(Map<Lesson, Integer> unsortMap, final boolean order)
+    {
+        List<Map.Entry<Lesson, Integer>> list = new LinkedList(unsortMap.entrySet());
+
+        list.sort((o1, o2) -> order ? o1.getValue().compareTo(o2.getValue()) == 0
+                ? o1.getKey().compareTo(o2.getKey())
+                : o1.getValue().compareTo(o2.getValue()) : o2.getValue().compareTo(o1.getValue()) == 0
+                ? o2.getKey().compareTo(o1.getKey())
+                : o2.getValue().compareTo(o1.getValue()));
+        return list.stream().collect(Collectors.toMap(Entry::getKey, Entry::getValue, (a, b) -> b, LinkedHashMap::new));
+
+    }
+
+    private static void printMap(Map<Lesson, Integer> map)
+    {
+        map.forEach((key, value) -> System.out.println(key.getDescription() + " : " + value));
     }
 }
