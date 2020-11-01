2ca80e02f3a8eac6a63f8f526752164b6654b2e6
Sergei Novikov <github@sergeynovi.ru>
2020-11-01 16:44:37 +0300

    Создаётся список с 1000 автомобилями различного типа с помощью метода make(). После создания на экран выводятся строки, содержащие: Количество машин каждой марки, количество машин в каждом сегменте (Regular / Premium).

--- 1/src/main/lesson9/task1/Factory.java
+++ 1/src/main/lesson9/task1/Factory.java
@@ -7,15 +7,15 @@ import java.util.Random;
 public class Factory {
 
     public static Car make() {
-        int randomChance = new Random().nextInt(100);
-        if (randomChance > 0 && randomChance < 40)
+        int randomChance = new Random().nextInt(100) + 1;
+        if (randomChance >= 1 && randomChance <= 40)
             return new Renault();
-        if (randomChance > 39 && randomChance < 70)
+        if (randomChance >= 41 && randomChance <= 70)
             return new Nissan();
-        if (randomChance > 69 && randomChance < 90)
+        if (randomChance >= 71 && randomChance <= 90)
             return new Mitsubishi();
-        if (randomChance > 89 && randomChance < 100)
+        if (randomChance >= 90 && randomChance <= 100)
             return new Mercedes();
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("Введено не верное случайное значение: " + randomChance);
     }
 }
