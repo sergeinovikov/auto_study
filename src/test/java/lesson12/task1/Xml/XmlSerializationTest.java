7be327b82f9214281e4ccea19902c18e909308cd
Sergei Novikov <github@sergeynovi.ru>
2020-11-28 17:28:44 +0300

    Задание 12.1. Сериализация и десириализация комнаты, квартиры и домы из JSON/XML файлов.

--- src/test/java/lesson12/task1/Xml/XmlSerializationTest.java
+++ src/test/java/lesson12/task1/Xml/XmlSerializationTest.java
@@ -1,15 +1,10 @@
 package lesson12.task1.Xml;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import main.lesson10.task4.entities.Property;
 import main.lesson12.task1.helpers.XmlHelper;
 import main.lesson12.task1.model.House;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class XmlSerializationTest {
