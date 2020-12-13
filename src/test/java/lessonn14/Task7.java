c4999ff8019ea9134c26b2de4eea07aedeca2a54
Sergei Novikov <github@sergeynovi.ru>
2020-12-13 18:32:09 +0300

    Задание 14.6 - Вывести на экран количество всех счетов, только рассчетных, только депозитных и толко текущих счетов людей. Убедиться, что суммарное количество счетов равно сумме расчетных, депозитных и текущих счетов

---
+++ src/test/java/lessonn14/Task7.java
@@ -0,0 +1,52 @@
+package lessonn14;
+
+import main.lesson14.helpers.JsonHelper;
+import main.lesson14.model.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+public class Task7 {
+    private final List<Person> persons = JsonHelper.getPersonsFromFile("1/src/main/lesson14/dataset.json");
+
+    @Test
+    public void personsAccountAmount() {
+        assert persons != null;
+        Long countPersonsAccounts = persons.stream()
+                .map(Person::getAccounts)
+                .filter(Objects::nonNull)
+                .mapToLong(Collection::size)
+                .sum();
+        System.out.println(countPersonsAccounts);
+        Assertions.assertEquals(988, countPersonsAccounts);
+
+        Long countCheckingAccounts = persons.stream()
+                .map(Person::getAccounts)
+                .flatMap(Collection::stream)
+                .filter(account -> account.getAccountType().equals("Расчетный"))
+                .count();
+        System.out.println(countCheckingAccounts);
+        Assertions.assertEquals(323, countCheckingAccounts);
+
+        Long countDepositoryAccounts = persons.stream()
+                .map(Person::getAccounts)
+                .flatMap(Collection::stream)
+                .filter(account -> account.getAccountType().equals("Депозитный"))
+                .count();
+        System.out.println(countDepositoryAccounts);
+        Assertions.assertEquals(342, countDepositoryAccounts);
+
+        Long countRunningAccounts = persons.stream()
+                .map(Person::getAccounts)
+                .flatMap(Collection::stream)
+                .filter(account -> account.getAccountType().equals("Текущий"))
+                .count();
+        System.out.println(countRunningAccounts);
+        Assertions.assertEquals(323, countRunningAccounts);
+
+        Assertions.assertEquals(countPersonsAccounts, countCheckingAccounts + countDepositoryAccounts + countRunningAccounts);
+    }
+}
